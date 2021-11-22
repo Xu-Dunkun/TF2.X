0556e7f7c3d46c0d551792b3360dd716061b8708
xudunkun <dunkun.xu@tcl.com>
2021-11-22 20:34:43 +0800

    feat: add baseline code

--- modelcompression/ParameterQuantization/wieghtclustering/clustering_total_model.py
+++ modelcompression/ParameterQuantization/wieghtclustering/clustering_total_model.py
@@ -1,26 +1,47 @@
 import tensorflow_model_optimization as tfmot
 import tensorflow as tf
+import pathlib
 
-train_images = 1
-train_labels = 2
+def main():
 
-cluster_weights = tfmot.clustering.keras.cluster_weights
-CentroidInitialization = tfmot.clustering.keras.CentroidInitialization
+    dirname = './tflite_weight_clustering'
+    filename = 'clustering_total_model.tflite'
+    train_images = '/modelmain/data/img_data'
+    train_labels = '/modelmain/data/img_label'
 
-clustering_params = {
-    'number_of_clusters': 16,
-    'cluster_centroids_init': CentroidInitialization.LINEAR
-}
+    model = tf.keras.models.load_model('trainandsave/model_h5/model.h5')
 
-clustered_model = cluster_weights(model, **clustering_params)
+    cluster_weights = tfmot.clustering.keras.cluster_weights
+    CentroidInitialization = tfmot.clustering.keras.CentroidInitialization
 
-# Use smaller learning rate for fine-tuning clustered model
-opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
-clustered_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        optimizer=opt,
-                        metrics=['accuracy'])
+    clustering_params = {
+        'number_of_clusters': 16,
+        'cluster_centroids_init': CentroidInitialization.LINEAR
+    }
 
-clustered_model.summary()
+    clustered_model = cluster_weights(model, **clustering_params)
 
-# Fine-tune model
-clustered_model.fit(train_images, train_labels, batch_size=500, epochs=1, validation_split=0.1)
+    # Use smaller learning rate for fine-tuning clustered model
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
+    clustered_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                            optimizer=opt,
+                            metrics=['accuracy'])
+
+    clustered_model.summary()
+
+    # Fine-tune model
+    clustered_model.fit(train_images, train_labels, batch_size=500, epochs=1, validation_split=0.1)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(clustered_model)
+    tflite_model = converter.convert()
+    save_tflite_model(tflite_model, dirname, filename)
+
+def save_tflite_model(tflite_model, dirname, filename):
+    tflite_models_dir = pathlib.Path(dirname)
+    tflite_models_dir.mkdir(exist_ok=True, parents=True)
+    tflite_model_file = tflite_models_dir + '/' + filename
+    tflite_model_file.write_bytes(tflite_model)
+    pass
+
+if __name__ == "__main__":
+    main()
